3bb00cf54eb332db252abe29ae4cccf50bc5f84b
cleytonfs777 <cleytonfs777@gmail.com>
2024-01-18 16:03:05 -0300

    Correcao paginas

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-function showFrame(frameId) {
-    document.querySelectorAll('.frame-content').forEach(div => {
-        div.style.display = 'none';
-    });
-    document.getElementById(frameId).style.display = 'block';
-}
